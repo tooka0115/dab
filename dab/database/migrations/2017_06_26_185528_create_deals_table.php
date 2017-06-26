c092975138a811c2c367980cfac0f9a64887b6df
Takeshi 00ka <takeshi.00ka@mopera.net>
2017-06-26 10:38:33 +0000

    deal model

---
+++ dab/database/migrations/2017_06_26_185528_create_deals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('year');
+            $table->integer('month');
+            $table->string('attribute');
+            $table->integer('categories_id');
+            $table->integer('item_id');
+            $table->integer('amount');
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('deals');
+    }
+}
