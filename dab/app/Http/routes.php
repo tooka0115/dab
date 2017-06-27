246eb1c10821b8812ddcee04a4604844e71248ef
Takeshi 00ka <takeshi.00ka@mopera.net>
2017-06-27 15:20:30 +0000

    top page

--- dab/app/Http/routes.php
+++ dab/app/Http/routes.php
@@ -10,7 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', 'DealsController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('deals', 'DealsController');
 
 
